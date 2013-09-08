187a14f61e8787a729a055438d6389b6b7b645cd
Texicitys <texicitys@gmail.com>
2013-09-08 17:45:08 +0200

    enlevé la liaison deliverymodes et payment modes. Utilisation des enum

--- app/models/advertisement/sale.rb
+++ app/models/advertisement/sale.rb
@@ -1,16 +1,19 @@
 # -*- encoding : utf-8 -*-
 class Advertisement::Sale < Advertisement::Advertisement
-  attr_accessible :currency, :price, :payment_mode_ids, :delivery_mode_ids
+  attr_accessible :currency, :price, :payment_modes, :delivery_modes
 
   extend Enumerize
-  enumerize :currency, in: %w[EUR CHF DOL], default: 'EUR', predicates: true
+  enumerize :currency, in: [:EUR, :CHF, :USD], default: :EUR, predicates: true
+  enumerize :payment_modes, in: [:cash, :bank, :check], multiple: true, predicates: true
+  enumerize :delivery_modes, in: [:hand, :post], multiple: true, predicates: true
 
-  has_and_belongs_to_many :payment_modes, :class_name => 'Advertisement::PaymentMode', include: :translations
-  has_and_belongs_to_many :delivery_modes, :class_name => 'Advertisement::DeliveryMode', include: :translations
+  #has_and_belongs_to_many :payment_modes, :class_name => 'Advertisement::PaymentMode', include: :translations
+  #has_and_belongs_to_many :delivery_modes, :class_name => 'Advertisement::DeliveryMode', include: :translations
 
-  accepts_nested_attributes_for :payment_modes
-  accepts_nested_attributes_for :delivery_modes
+  #accepts_nested_attributes_for :payment_modes
+  #accepts_nested_attributes_for :delivery_modes
 
 
   validates_presence_of :price, numericality: true, inclusion: {in: 0..300}
+  validates_presence_of :currency
 end
--- app/models/advertisement/sale_book.rb
+++ app/models/advertisement/sale_book.rb
@@ -3,8 +3,8 @@ class Advertisement::SaleBook < Advertisement::Sale
   attr_accessible :state, :annotation, :domain_ids
 
   extend Enumerize
-  enumerize :annotation, in: %w[no_annotation annotations], predicates: true
-  enumerize :state, in: %w[new like_new old], predicates: true
+  enumerize :annotation, in: [:not_annotated, :slightly_annotated, :annotated, :very_annotated], predicates: true
+  enumerize :state, in: [:new, :like_new, :normal, :damaged, :very_damaged], predicates: true
 
   #Liaisons
   belongs_to :book
