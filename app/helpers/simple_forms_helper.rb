2aa2275cd997cd9e33af3c6adc0ec8b23fd6bce3
Texicitys <texicitys@gmail.com>
2013-09-12 03:40:35 +0200

    corrigé bug tooltips

--- app/helpers/simple_forms_helper.rb
+++ app/helpers/simple_forms_helper.rb
@@ -3,6 +3,6 @@ module SimpleFormsHelper
   #permet d'introduire facilement un tooltip dans un formulaire
   #=f.input :annotation, label_html: tooltip('salut')
   def tooltip(tip, position = 'tip-top')
-    {class: "has-tip #{position}", title: tip, 'data-tooltip' => true}
+    {class: "has-tip #{position}", title: tip, 'data-tooltip' => ''}
   end
 end
