0f2e7586fb214396409c8c1714bf36db3708865e
Texicitys <texicitys@gmail.com>
2013-09-18 00:41:00 +0200

    galaire à faire un joli visuel pour la messagerie

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,15 @@
 # -*- encoding : utf-8 -*-
 class MessagesController < ApplicationController
+  add_breadcrumb I18n.t("messages.index.breadcrumb"),  :messages_path
+  add_breadcrumb I18n.t("messages.new.h1_title"), :new_message_path, :only => %w(new create)
+
   def index
+    current_user.send_message(current_user, "Salut Yves, je t'envoie ce mail car j'ai un soucis", 'titre')
+
+    @conversations = getConversations
     #message = message.new({:body => 'msg_body', :subject => 'subject', :attachment => 'attachment'})
     #user.send_message(user2, 'messagbody', 'messagesubject')
-    @messages = current_user.mailbox.conversations
+    #@conversations = current_user.mailbox.inbox
 
     ##alfa wants to retrieve all his conversations
     #alfa.mailbox.conversations
@@ -18,39 +24,44 @@ class MessagesController < ApplicationController
     #alfa.mailbox.trash
   end
 
+  #def show
+  #  @conversation = Conversation.find(params[:id])
+  #  @receipts = @conversation.receipts_for current_user
+  #end
+
   def new
+    @conversations = getConversations
     @message = MessageSending.new
     #current_user.send_message(current_user, '@message.body', '@message.subject')
   end
 
   def create
-    @message = Message.new(params[:message])
-
-    if @message.valid?
-      ##On ajoute tous les user receveur dans un array
-      #receivers = Array.new
-      #@message.users.each do |u|
-      #  receivers << u.id
-      #end
-      current_user.send_message(@message.users, @message.body, @message.subject)
-      format.html { redirect_to @message, notice: t('messages.new.forms.succes') }
-      format.json { render json: @message, status: :created, location: @message }
-    else
-      format.html { render action: "new" }
-      format.json { render json: @message.errors, status: :unprocessable_entity }
+    @conversations = getConversations
+    @message = MessageSending.new(params[:message_sending])
+    respond_to do |format|
+      if @message.valid?
+        current_user.send_message(@message.users, @message.body, @message.subject)
+        format.html { redirect_to :messages, notice: t('messages.new.forms.success') }
+        format.json { render json: :messages, status: :created, location: @message }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
     end
-end
+  end
 
-def destroy
-end
+  def destroy
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-end
+  def update
+  end
 
-def show
-end
+  private
+  def getConversations
+    current_user.mailbox.conversations
+  end
 
 end
--- app/models/message_sending.rb
+++ app/models/message_sending.rb
@@ -5,10 +5,14 @@ class MessageSending < ActiveRecord::Base
     columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
   end
 
-  column :subject
-  column :body
+  attr_accessible :subject, :body, :user_ids
+
+  column :subject, :string
+  column :body, :text
 
   validates_presence_of :body
+  validates_presence_of :subject
+  validates_presence_of :users
 
   has_and_belongs_to_many :users
 end
