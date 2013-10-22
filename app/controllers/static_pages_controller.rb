9277308889b4966512778fa475b288fffab04d14
leo <facenord.sud@gmail.com>
2013-10-23 00:33:57 +0200

    avancé à la page d'accueil

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,20 +1,27 @@
 # -*- encoding : utf-8 -*-
 class StaticPagesController < ApplicationController
-
   def home
     unless params[ :locale]
       # it takes I18n.locale from the previous example set_locale as before_filter in application controller
       redirect_to eval("root_#{I18n.locale}_path")
     end
+    @documents = Document::Document.order('updated_at ASC').limit(10).all
+    @ads = Advertisement::Advertisement.order('updated_at ASC').limit(10).includes(:book).all
+    site_news = SiteNew.new
+    @news = site_news.get_recent_posts(count: 10).get(:posts)
+    add_breadcrumb(t('static_pages.home.bc'))
   end
 
   def help
+    add_breadcrumb(t('static_pages.help.bc'))
   end
 
   def about
+    add_breadcrumb(t('static_pages.about.bc'))
   end
 
   def contact
+    add_breadcrumb(t('static_pages.contact.bc'))
   end
 
   def change_lang
---
+++ app/models/site_new.rb
@@ -0,0 +1,65 @@
+class SiteNew
+  require 'net/http'
+  attr_accessor :host
+
+  def initialize(host='')
+    host.blank? ? @host = 'blog-oj-laneuveville.herokuapp.com' : @host = host
+    @methods = {}
+    @methods[:get_recent_posts] = 'get_recent_posts'
+  end
+
+  def get_recent_posts(options = {})
+    execute(@methods[:get_recent_posts], options)
+  end
+
+  private
+    def execute(method, options={})
+      more = ''
+      more = options.map {|an_option, a_value| "#{an_option}=#{a_value}"}.join('&')
+      more = '&' + more unless more.blank?
+      JsonResponse.new Net::HTTP.get(host, "/?json=#{method}#{more}")
+    end
+end
+
+class JsonResponse
+  attr_accessor :json_text, :json_hash
+
+  def initialize(json)
+    @json_text = json
+    @json_hash = JSON.parse(json_text)
+  end
+
+  def get(sym)
+    if sym == :posts
+      return get_posts json_hash[sym.to_s]
+    end
+    json_hash[sym.to_s]
+  end
+
+  def json=(json)
+    @json_text = json
+    @json_hash = JSON.parse(json_text)
+  end
+
+  def status
+    get :status
+  end
+
+  private
+    def get_posts(hash)
+      hash.map do |a_post|
+        p = Post.new
+        p.title = a_post['title']
+        p.url = a_post['url']
+        p.id = a_post['id']
+        p.slug = a_post['slug']
+        p.status = a_post['status']
+        p.content = a_post['content']
+        p
+      end
+    end
+end
+
+class Post
+  attr_accessor :title, :url, :id, :slug, :status, :title_plain, :content, :excerpt, :date, :date_modified
+end
