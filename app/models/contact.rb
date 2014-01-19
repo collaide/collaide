e4dadcf9fcb68a789ae952a28d39eb96de3be899
Texicitys <texicitys@gmail.com>
2014-01-19 17:35:04 +0100

    Test, contact, pas réussi

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,16 @@
 # -*- encoding : utf-8 -*-
 class Contact < ActiveRecord::Base
+  def self.columns() @columns ||= []; end
+
+  def self.column(name, sql_type = nil, default = nil, null = true)
+    columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
+  end
+
   attr_accessible :content, :email, :subject
+
+  column :subject, :string
+  column :email, :string
+  column :content, :text
 
   validates :subject, presence: true
   validates :email, presence: true
