c59cf7e83a5804bb84e1409b3674a1aefbfbe8d0
Texicitys <texicitys@gmail.com>
2014-01-30 04:15:48 +0100

    On puet se connecter via FB et google

--- app/models/user.rb
+++ app/models/user.rb
@@ -135,17 +135,24 @@ class   User < ActiveRecord::Base
     end
   end
 
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
-
-    unless user
-      user = User.create(name: data["name"],
-                         email: data["email"],
-                         password: Devise.friendly_token[0,20]
-      )
+  def self.find_for_google_oauth2(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+      user.save!
     end
-    user
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
   end
 
   #has_many :authorizations
