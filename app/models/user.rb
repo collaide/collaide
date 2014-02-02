f1d17fefa1c7b638fe21367677f928b3681f168d
leo <facenord.sud@gmail.com>
2014-02-03 00:34:19 +0100

    déploiment production

--- app/models/user.rb
+++ app/models/user.rb
@@ -142,7 +142,7 @@ class   User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.remote_avatar_url = auth.info.image # assuming the user model has an image
       user.save!
     end
   end
