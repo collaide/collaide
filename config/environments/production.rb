54a8bdd245e18695e361f7d6ded41174a84b1554
leo <facenord.sud@gmail.com>
2014-02-03 00:26:15 +0100

    déploiment production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,6 @@ Collaide::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = {:host => "vps42634.ovh.net"}
+
 end
