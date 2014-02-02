f85530a68a5965d5ecb85ed2be06c07149242189
leo <facenord.sud@gmail.com>
2014-02-03 00:38:08 +0100

    déploiment production

--- app/uploaders/user_uploader.rb
+++ app/uploaders/user_uploader.rb
@@ -24,7 +24,7 @@ class UserUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-     %w(jpg jpeg gif png)
+     %w(jpg jpeg gif png JPG JPEG)
   end
 
   # Override the filename of the uploaded files:
