23d7811131236c2b08b92d10cda3344f4567677d
Texicitys <texicitys@gmail.com>
2014-03-09 16:55:42 +0100

    Adapté aux nouveaux noms des fonctions de repository manager

--- app/controllers/group/repo_items_controller.rb
+++ app/controllers/group/repo_items_controller.rb
@@ -68,7 +68,7 @@ class Group::RepoItemsController < ApplicationController
       send_file_method = :default
     end
 
-    path = @group.download(@repo_item)
+    path = @group.download_repo_item(@repo_item)
     logger.debug path.inspect
 
     # Si le fichier n'est pas trouvé
@@ -124,7 +124,7 @@ class Group::RepoItemsController < ApplicationController
     logger.debug @repo_item.inspect
     logger.debug target.inspect
     respond_to do |format|
-      if @group.copy_repo_item(@repo_item, target)
+      if @group.copy_repo_item(@repo_item, source_folder: target)
         format.json { render :show }
       else
         format.json { render json: @repo_item.errors, status: :unprocessable_entity }
@@ -136,7 +136,7 @@ class Group::RepoItemsController < ApplicationController
     move_params = params.require(:repo_item).permit :id
     target = RepositoryManager::RepoFolder.find move_params[:id]
     respond_to do |format|
-      if @group.move_repo_item @repo_item, target
+      if @group.move_repo_item @repo_item, source_folder: target
         format.json { render :show }
       else
         format.json { render json: @repo_item.errors, status: :unprocessable_entity }
