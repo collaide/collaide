5866b72dcae7c4d96507722c57991d426de53f91
facenord-sud <facenord.sud@gmail.com>
2014-05-05 08:43:15 +0200

    INSERT notification marche si redis est désactivé

--- app/notifications/notification_system/abstract_class.rb
+++ app/notifications/notification_system/abstract_class.rb
@@ -3,8 +3,12 @@ class NotificationSystem::AbstractClass < ActionView::Base
   include Rails.application.routes.url_helpers
 
   def self.perform_later(method, values, options)
-    Resque.enqueue(NotificationSystem::Save, self.to_s, method, values, options)
-    #NotificationSystem::Save.perform(self.to_s, method, values, options)
+    begin
+      Resque.enqueue(NotificationSystem::Save, self.to_s, method, values, options)
+    rescue Errno::ECONNREFUSED, Redis::CannotConnectError
+      Rails.logger.error "Unable to connect to Redis; falling back to synchronous insert notification" if Rails.logger
+            deliver!
+    end
   end
 
 end
