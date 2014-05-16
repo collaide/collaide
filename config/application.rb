544457f52b1a71dcf37d71b2c15b2f4f4db8a2dd
Texicitys <texicitys@gmail.com>
2014-05-16 09:48:55 +0200

    Ajout des pages d'erreurs non static

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,7 @@ module Collaide
     config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td'
     config.action_view.sanitized_allowed_attributes = ['style']
 
-    # TODO faire ces propres pages d'erreurs
-    #config.exceptions_app = self.routes
+    config.exceptions_app = self.routes
 
     # Custom logging
   # Enable the logstasher logs for the current environment
