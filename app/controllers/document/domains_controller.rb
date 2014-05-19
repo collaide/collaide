50f073ba46bb75b17c0c7881027d2ec1e0ee4cb4
leo <facenord.sud@gmail.com>
2014-05-19 21:41:57 +0200

    correction bug

--- app/controllers/document/domains_controller.rb
+++ app/controllers/document/domains_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Document::DomainsController < ApplicationController
-  load_and_authorize_resource class: Document::Domain
+  #load_and_authorize_resource class: Document::Domain
   def show
     @domain = Domain.find params[:id]
   end
