1f899850d3a4bc811d5c89e9184b909397ccd47a
leo <facenord.sud@gmail.com>
2014-06-04 00:52:44 +0200

    correction bug #94

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,9 @@ class CommentsController < ApplicationController
   include PolymorphicsController
   def create
     get_object(comment_params, 'comments', {comment: comment_params[:comment], title: comment_params[:title], owner: current_user}) do |object|
+      logger.debug 'is_a? ' + object.is_a?(Topic).inspect
+      logger.debug 'owner' + object.owner.is_a?(Group::Group).inspect
+      logger.debug 'can?' + object.owner.can_not?(:write, :topic, current_user).inspect
       object.is_a? Topic and object.owner.is_a? Group::Group and object.owner.can_not? :write, :topic, current_user
     end
   end
