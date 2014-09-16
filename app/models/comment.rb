3a90abf904b91cec330ed8b353cbb01b66e1dac9
facenord-sud <facenord.sud@gmail.com>
2014-09-16 18:33:38 +0200

    conversations dans le bonne ordre

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,9 +20,9 @@ class Comment < ActiveRecord::Base
 
   include ActsAsCommentable::Comment
 
-  belongs_to :commentable, :polymorphic => true
+  belongs_to :commentable, :polymorphic => true, touch: true
 
-  default_scope -> {order('created_at DESC')}
+  default_scope -> {order('created_at ASC')}
 
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
